b332bc872849fd48b99c2621f5405e98e64d53c0
ivangoel <76724871+ivangoel7@users.noreply.github.com>
2021-03-21 18:23:41 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,35 +1,30 @@
-class paper
-{
-	constructor(x,y,r)
-	{
-		var options={
-			isStatic:false,
-			restitution:0.3,
-			friction:0.5,
-			density:1.2
+class Paper{
+constructor(x,y,r){
+    var options ={
+        isStatic: false,
+        restitution: 0.3,
+        friction: 0,
+        density: 1.2
+    }
+    this.x=x;
+    this.y=y;
+    this.r=r;
+    this.body = Bodies.circle(this.x,this.y,60,options);
+    World.add(world,this.body);
+    this.image =loadImage("paper.png");
+    
+}
+display(){
+    var pos=this.body.position;
+    push()
+    translate(pos.x, pos.y);
+    ellipseMode(RADIUS);
+    //ellipse(0,0,20,20);
+    image(this.image,0,0,this.r, this.r);
+    
+    pop();
+}
 
-			
-			}
-		this.x=x;
-		this.y=y;
-		this.r=r
-		this.body=Bodies.circle(this.x, this.y, this.r/2, options)
-		World.add(world, this.body);
 
-	}
-	display()
-	{
-			
-			var paperpos=this.body.position;		
-
-			push()
-			translate(paperpos.x, paperpos.y);
-			rectMode(CENTER)
-			strokeWeight(3);
-			fill(255,0,255)
-			ellipse(0,0,this.r, this.r);
-			pop()
-			
-	}
 
 }
