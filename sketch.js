7444b6098fe0ba870b30f77b03a08c93b35c3dca
ivangoel <76724871+ivangoel7@users.noreply.github.com>
2021-03-21 18:27:23 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,57 +3,42 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-var dustbinObj, paperObject,groundObject	
-var world;
-
+var dustbinObj,groundObject	
+var engine,world;
+var ball;
 
 function setup() {
-	createCanvas(1600, 700);
+	createCanvas(1600, 600);
 	rectMode(CENTER);
 
 
 	engine = Engine.create();
 	world = engine.world;
-	dustbinObj=new dustbin(1200,650);
-	paperObject=new paper(200,450,40);
-	groundObject=new ground(width/2,670,width,20);
-	//Create a Ground
 	
+	groundObject=new ground(width/2,570,width,20);
+	dustbinObj=new dustbin(1200,550);
+	ball = new Paper(200,450,70);
 
-	var render = Render.create({
-	  element: document.body,
-	  engine: engine,
-	  options: {
-	    width: 1200,
-	    height: 700,
-	    wireframes: false
-	  }
-	});
-
+	
 	Engine.run(engine);
-	//Render.run(render);
+	
   
 }
 
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(230);
  
-  dustbinObj.display();
-  paperObject.display();
+
   groundObject.display();
-  
- 
-  
-  
- 
+  dustbinObj.display();
+  ball.display();
 }
 
-function keyPressed() {
-  	if (keyCode === UP_ARROW) {
-
-    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-    
-  	}
+function keyPressed(){
+	if(keyCode === UP_ARROW){
+		
+		Matter.Body.applyForce(ball.body,ball.body.position,{x:830,y:-775});
+	}
 }
